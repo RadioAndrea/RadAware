db31ebfa48ac6196491a1aa6a9f18500717a0bf9
RadioAndrea <andreancowley@gmail.com>
2016-04-16 14:32:32 -0400

    Added drawables

--- app/src/main/java/radioandrea/net/radaware/MainActivity.java
+++ app/src/main/java/radioandrea/net/radaware/MainActivity.java
@@ -28,7 +28,7 @@ import java.util.TimerTask;
 public class MainActivity extends AppCompatActivity {
 
     private Uri outputFileUri;
-    private ImageView pictureView;
+    private ImageView imageView;
     private static final int TAKE_PICTURE = 0;
     private final File file = new File(Environment.getExternalStorageDirectory(), "implicit.jpg");
     private final File outputFile = new File(Environment.getExternalStorageDirectory(), "share.jpg");
@@ -45,8 +45,8 @@ public class MainActivity extends AppCompatActivity {
 
         Button button = (Button) findViewById(R.id.button);
         TextView textView = (TextView) findViewById(R.id.gammaDisplay);
-        ImageView imageView = (ImageView) findViewById(R.id.imageView);
-
+        imageView = (ImageView) findViewById(R.id.imageView);
+        scaleFactor = 0.8;
 
     }
 
@@ -69,8 +69,10 @@ public class MainActivity extends AppCompatActivity {
 
     private void takepicture(int resultCode) {
         if (resultCode == RESULT_OK) {
-            Bitmap bitmap = Camera_Helpers.loadAndScaleImage(file.getAbsolutePath(), (int) (pictureView.getHeight() * scaleFactor), (int) (pictureView.getWidth() * scaleFactor));
-            pictureView.setImageBitmap(bitmap);
+            int height = 100;
+            int width = 100;
+            Bitmap bitmap = Camera_Helpers.loadAndScaleImage(file.getAbsolutePath(), height, width);
+            imageView.setImageBitmap(bitmap);
             Camera_Helpers.saveProcessedImage(bitmap, file.getAbsolutePath());
         }
     }
