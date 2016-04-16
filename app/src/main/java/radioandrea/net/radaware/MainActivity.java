d2aa289eb4bd5a909868ca1eab12d0dac18a2d43
RadioAndrea <andreancowley@gmail.com>
2016-04-16 15:49:02 -0400

    It works

--- app/src/main/java/radioandrea/net/radaware/MainActivity.java
+++ app/src/main/java/radioandrea/net/radaware/MainActivity.java
@@ -1,88 +1,132 @@
 package radioandrea.net.radaware;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.hardware.Camera;
-import android.net.Uri;
-import android.os.Environment;
-import android.os.IBinder;
-import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
+import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements SurfaceHolder.Callback {
+    Camera camera;
+    SurfaceView surfaceView;
+    SurfaceHolder surfaceHolder;
+    TextView textView2;
 
-    private ImageView imageView;
-    private int colorId = 0;
-    private static final int BLUR_RADIUS = 4;
-    static Bitmap largeIcon;
 
-    private Uri outputFileUri;
-    private ImageView pictureView;
-    private static final int TAKE_PICTURE = 0;
-    private final File file = new File(Environment.getExternalStorageDirectory(), "implicit.jpg");
-    private final File outputFile = new File(Environment.getExternalStorageDirectory(), "share.jpg");
-    private double scaleFactor;
-    private int sketch;
-    private int color;
-    private String subjectString;
-    private String textString;
-    TextView textView;
+    Camera.PictureCallback rawCallback;
+    Camera.ShutterCallback shutterCallback;
+    Camera.PictureCallback jpegCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.button);
-        textView = (TextView) findViewById(R.id.gammaDisplay);
-        imageView = (ImageView) findViewById(R.id.imageView);
-        scaleFactor = 0.8;
+        textView2 = (TextView) findViewById(R.id.textView2);
+
+        surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+        surfaceHolder = surfaceView.getHolder();
+
+        surfaceHolder.addCallback(this);
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        jpegCallback = new PictureCallback() {
+
+            @Override
+            public void onPictureTaken(byte[] data, Camera camera) {
+
+                Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+                textView2.setText(safetyZone(listLum(bitmap)));
+
+
+                Toast.makeText(getApplicationContext(), "Picture Run", Toast.LENGTH_LONG).show();
+                refreshCamera();
+            }
+        };
+    }
+
+    public void captureImage(View v) throws IOException {
+        camera.takePicture(null, null, jpegCallback);
+
 
     }
 
-    public void launchCameraApp(View v) {
-        // create intent to take picture with camera and specify storage
-        // location so we can easily get it
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        outputFileUri = Uri.fromFile(file);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
-        startActivityForResult(intent, TAKE_PICTURE);
-    }
+    public void refreshCamera() {
+        if (surfaceHolder.getSurface() == null) {
+            return;
+        }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            case (TAKE_PICTURE):
-                takepicture(resultCode);
-                break;
+        try {
+            camera.stopPreview();
+        }
+
+        catch (Exception e) {
+        }
+
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+        }
+        catch (Exception e) {
         }
     }
 
-    private void takepicture(int resultCode) {
-        if (resultCode == RESULT_OK) {
-            int height = 100;
-            int width = 100;
-            Bitmap bitmap = Camera_Helpers.loadAndScaleImage(file.getAbsolutePath(), height, width);
-            //imageView.setImageBitmap(bitmap);
-            textView.setText(safetyZone(listLum(bitmap)));
-            Camera_Helpers.saveProcessedImage(bitmap, file.getAbsolutePath());
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            camera = Camera.open();
         }
+
+        catch (RuntimeException e) {
+            System.err.println(e);
+            return;
+        }
+
+        Camera.Parameters param;
+        param = camera.getParameters();
+        param.setPreviewSize(352, 288);
+        camera.setParameters(param);
+
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+        }
+
+        catch (Exception e) {
+            System.err.println(e);
+            return;
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        refreshCamera();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        camera.stopPreview();
+        camera.release();
+        camera = null;
     }
 
     public static double listLum(Bitmap bm) {
@@ -95,10 +139,11 @@ public class MainActivity extends AppCompatActivity {
         double g = 0.0;
         double b = 0.0;
 
-        int totalPix = bm.getWidth() * bm.getHeight();
+        int skip = 5;
+        int totalPix = bm.getWidth() * bm.getHeight() / skip;
 
-        for (int x = 0; x < bm.getWidth(); x++) {
-            for (int y = 0; y < bm.getHeight(); y++) {
+        for (int x = 0; x < (bm.getWidth()-skip); x+=skip) {
+            for (int y = 0; y < (bm.getHeight()-skip); y+=skip) {
                 int colour = bm.getPixel(x, y);
 
                 red = Color.red(colour);
@@ -129,8 +174,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public static String safetyZone(double lum) {
-        int cutoff = 7000;
-        int warning = 5000;
+        int cutoff = 300;
+        int warning = 100;
 
         if (lum >= cutoff) {
             return "Fail!!!";
